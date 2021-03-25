09edac79ed54c6b266d5f965d57c4bd245d5dacb
szczynk <szczynk@users.noreply.github.com>
2021-03-25 19:05:03 +0000

    deploy: 23ecba90a82a95b1018d29d9c4540a2982bbbcfd

--- _nuxt/static/1616698306/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fresume\u002F_nuxt\u002Fstatic\u002F1616698306",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{app:{basePath:"\u002Fresume\u002F",assetsPath:"\u002Fresume\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"192023ac"}}}}(null));
