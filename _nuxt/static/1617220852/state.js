f91827df865ae3b1bea381efbdf1a842668be4f1
sanguinary-bus <sanguinary-bus@users.noreply.github.com>
2022-06-07 13:22:56 +0000

    deploy: 7b586b730ff8e727a7c952fe0031a1886617f69c

--- _nuxt/static/1617220852/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fresume\u002F_nuxt\u002Fstatic\u002F1617220852",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{app:{basePath:"\u002Fresume\u002F",assetsPath:"\u002Fresume\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7e86bf51"}}}}(null));
